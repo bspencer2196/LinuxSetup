847c4d4616ab072af88868bd86d11fe0f2b05ee0
Bradley Spencer <bradleymspencer21@gmail.com>
2022-05-16 23:29:59 -0500

    Separating different installs for server

---
+++ osint_tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+mkdir OtherOsintTools
+cd OtherOsintTools
+git clone https://github.com/twintproject/twint.git
+git clone https://github.com/Datalux/Osintgram.git
+git clone https://github.com/laramies/theHarvester.git
---
+++ setup_desktop_forVNC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt -y install xfe4 xfce4-goodies tightvncserver
+
+sudo apt-get install gnome-core desktop-base -y
+sudo apt -y install build-essential git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev libjansson-dev libxml2-dev uuid-dev default-libmysqlclient-dev
+
+
+
